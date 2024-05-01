5e951aa8fbf9ae4f95c556382bab724585498bba
gabrprog <g.alberto.dev@gmail.com>
2024-04-30 23:23:56 -0300

    Exercício 10

--- br/edu/up/Ex10.java
+++ br/edu/up/Ex10.java
@@ -1,3 +1,25 @@
-public class Ex10 {
+package br.edu.up;
 
+import java.util.Scanner;
+
+public class Ex10 {
+    public static void executar() {
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Digite o número de pessoas: ");
+        int numeroPessoas = leitor.nextInt();
+        
+        for (int i = 1; i <= numeroPessoas; i++) {
+            System.out.print("Digite a idade da pessoa " + i + ": ");
+            int idade = leitor.nextInt();
+            
+            if (idade >= 18) {
+                System.out.println("Pessoa " + i + " é maior de idade.");
+            } else {
+                System.out.println("Pessoa " + i + " é menor de idade.");
+            }
+        }
+        
+        leitor.close();
+    }
 }
