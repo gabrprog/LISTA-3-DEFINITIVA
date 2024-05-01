f5e714301b8d7e83648324c3ae1e9d8dd3e43285
gabrprog <g.alberto.dev@gmail.com>
2024-04-30 23:57:32 -0300

    Exercício 22

---
+++ br/edu/up/Ex22.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Ex22 {
+    public static void executar() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Informe o tipo de cliente:");
+        System.out.println("1 - Residência");
+        System.out.println("2 - Comércio");
+        System.out.println("3 - Indústria");
+        int tipoCliente = scanner.nextInt();
+        
+        double valorKwh;
+        switch (tipoCliente) {
+            case 1:
+                valorKwh = 0.60;
+                break;
+            case 2:
+                valorKwh = 0.48;
+                break;
+            case 3:
+                valorKwh = 1.29;
+                break;
+            default:
+                System.out.println("Tipo de cliente inválido.");
+                return;
+        }
+        
+        System.out.println("Digite o consumo em kWh: ");
+        double consumo = scanner.nextDouble();
+        
+        double valorConta = consumo * valorKwh;
+        
+        System.out.println("O valor da conta de luz é: R$" + valorConta);
+        
+    }
+}
