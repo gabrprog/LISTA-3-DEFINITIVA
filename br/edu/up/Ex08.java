370c4a42767922bf65003964b10bbe2285002e55
gabrprog <g.alberto.dev@gmail.com>
2024-04-30 23:20:47 -0300

    Exercício 8

--- br/edu/up/Ex08.java
+++ br/edu/up/Ex08.java
@@ -1,3 +1,38 @@
-public class Ex08 {
+package br.edu.up;
 
+import java.util.Scanner;
+
+public class Ex08 {
+    public static void executar() {
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Digite o nome do aluno: ");
+        String nome = leitor.nextLine();
+        
+        System.out.println("Digite a primeira nota: ");
+        double nota1 = leitor.nextDouble();
+        
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = leitor.nextDouble();
+        
+        System.out.println("Digite a terceira nota: ");
+        double nota3 = leitor.nextDouble();
+        
+        double media = (nota1 + nota2 + nota3) / 3;
+        
+        String mencao;
+        if (media >= 7.0) {
+            mencao = "Aprovado";
+        } else if (media <= 5.0) {
+            mencao = "Reprovado";
+        } else {
+            mencao = "Recuperação";
+        }
+        
+        System.out.println("Nome do aluno: " + nome);
+        System.out.println("Média: " + media);
+        System.out.println("Menção: " + mencao);
+        
+        leitor.close();
+    }
 }
--- br/edu/up/Programa.java
+++ br/edu/up/Programa.java
@@ -4,10 +4,10 @@ public class Programa {
     public static void main(String[] args) {
        
         //Ex01.executar();
-       
+        //Ex02.executar();
         //Ex03.executar();
         //Ex04.executar();
-        //  Ex05.executar();
+        // Ex05.executar();
         // Ex06.executar();
         //Ex07.executar();
 
