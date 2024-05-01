3c78771b9dec0b04bc9a6947d6bae180f504c103
gabrprog <g.alberto.dev@gmail.com>
2024-04-30 23:08:48 -0300

    Exercício 6

--- br/edu/up/Programa.java
+++ br/edu/up/Programa.java
@@ -4,7 +4,7 @@ public class Programa {
     public static void main(String[] args) {
         //Ex03.executar();
         //Ex04.executar();
-          Ex05.executar();
+        //  Ex05.executar();
 
 
 
