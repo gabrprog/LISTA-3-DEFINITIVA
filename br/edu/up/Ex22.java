4746b47c8a711d9373c778be345d060f744e0442
gabrprog <g.alberto.dev@gmail.com>
2024-05-01 00:05:58 -0300

    Exercício 22

--- br/edu/up/Ex22.java
+++ br/edu/up/Ex22.java
@@ -1,37 +1,47 @@
+package br.edu.up;
+
 import java.util.Scanner;
 
 public class Ex22 {
     public static void executar() {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.println("Informe o tipo de cliente:");
         System.out.println("1 - Residência");
         System.out.println("2 - Comércio");
         System.out.println("3 - Indústria");
         int tipoCliente = scanner.nextInt();
-        
-        double valorKwh;
-        switch (tipoCliente) {
-            case 1:
-                valorKwh = 0.60;
-                break;
-            case 2:
-                valorKwh = 0.48;
-                break;
-            case 3:
-                valorKwh = 1.29;
-                break;
-            default:
-                System.out.println("Tipo de cliente inválido.");
-                return;
+
+        double valorKwh = obterValorKwh(tipoCliente);
+        if (valorKwh == -1) {
+            System.out.println("Tipo de cliente inválido.");
+            return;
         }
-        
+
         System.out.println("Digite o consumo em kWh: ");
         double consumo = scanner.nextDouble();
-        
-        double valorConta = consumo * valorKwh;
-        
+
+        double valorConta = calcularValorConta(consumo, valorKwh);
+
         System.out.println("O valor da conta de luz é: R$" + valorConta);
-        
+
+        scanner.close();
+    }
+
+    private static double obterValorKwh(int tipoCliente) {
+        switch (tipoCliente) {
+            case 1:
+                return 0.60;
+            case 2:
+                return 0.48;
+            case 3:
+                return 1.29;
+            default:
+                return -1; // Retorna -1 se o tipo de cliente for inválido
+        }
+    }
+
+    private static double calcularValorConta(double consumo, double valorKwh) {
+        return consumo * valorKwh;
     }
 }
